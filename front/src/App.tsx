bcf1fa90e995a1c7ffe81e5de8cec57b419710fd
maciek <macieq2048@gmail.com>
2022-05-17 22:10:42 +0200

    dupaaaaa

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,7 +1,8 @@
-import { FC, useMemo, useState } from 'react';
+import {FC, useMemo, useRef, useState} from 'react';
 import CryingLaughSrc from './assets/crying_laugh.png';
 import RedditSettings from './RedditSettings';
 import WielkiButon from './ui/WielkiButon';
+import videosrc from './assets/youtube_short_2.webm';
 
 const generateRandomCryingEmoji = (count: number, hidden: boolean) => {
   return (
@@ -36,6 +37,8 @@ function App() {
     [cryingEmojiCount, showCryingEmoji]
   );
 
+  const videoSrc = useRef(null);
+
   const renderCryingEmojiSlider = () => (
     <div className={`w-80 ${!showCryingEmoji ? 'hidden' : 'visible'}`}>
       <input
@@ -55,19 +58,51 @@ function App() {
     </div>
   );
 
+  const generateVideo = async () => {
+    const res = await fetch("http://127.0.0.1:5000/api/videos/create?subreddit=dankmemes", {method: 'POST'})
+    const blob = await res.blob();
+    const temp = URL.createObjectURL(blob);
+    // videoSrc.current.load();
+    console.log("res", videoSrc.current, temp)
+    display(blob)
+  }
+
+  function display( videoFile ) {
+
+    // Preconditions:
+    if( !( videoFile instanceof Blob ) ) throw new Error( '`videoFile` must be a Blob or File object.' ); // The `File` prototype extends the `Blob` prototype, so `instanceof Blob` works for both.
+    if( !( videoSrc.current instanceof HTMLVideoElement ) ) throw new Error( '`videoEl` must be a <video> element.' );
+
+    //
+
+    const newObjectUrl = URL.createObjectURL( videoFile );
+
+    // URLs created by `URL.createObjectURL` always use the `blob:` URI scheme: https://w3c.github.io/FileAPI/#dfn-createObjectURL
+    const oldObjectUrl = videoSrc.current.currentSrc;
+    if( oldObjectUrl && oldObjectUrl.startsWith('blob:') ) {
+      // It is very important to revoke the previous ObjectURL to prevent memory leaks. Un-set the `src` first.
+      // See https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL
+
+      videoSrc.current.src = ''; // <-- Un-set the src property *before* revoking the object URL.
+      URL.revokeObjectURL( oldObjectUrl );
+    }
+
+    // Then set the new URL:
+    videoSrc.current.src = newObjectUrl;
+    videoSrc.current.src
+
+    // And load it:
+    videoSrc.current.load(); // https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/load
+
+  }
+
+
+
   return (
     <div>
-      <div className="grid w-screen h-screen space-y-32 text-3xl bg-indigo-200 place-content-center">
-        <div onMouseOver={() => setShowCryingEmoji(true)}>
-          <WielkiButon>
-            przycisk gigantyczny wielki ze potezny i mozna wygenerowac filmik
-            fajny po prostu a okej
-          </WielkiButon>
-        </div>
-        <RedditSettings />
-        {renderCryingEmojiSlider()}
-      </div>
-      {randomCryingEmoji}
+      <video controls autoPlay={true} src={videosrc} height={'500'}>
+        {/*<source src={videosrc} type={"video/mp4"} height="500px"/>*/}
+      </video>
     </div>
   );
 }
