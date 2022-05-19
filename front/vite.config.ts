da196ac9fb773dc82a154d75c80d3f5ded569e42
jan-czerwinski <jan.ignacy.czerwinski@gmail.com>
2022-05-19 13:44:27 +0200

    fix vite

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    hmr: {
-      host: '0.0.0.0',
-      port: 3010,
-    },
     host: '0.0.0.0',
     port: 3000,
   },
